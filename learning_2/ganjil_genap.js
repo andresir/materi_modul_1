c3ac044f690e0b08bef5c7473a984bc90b162ffd
andresir <bisnis.ir@gmail.com>
2018-11-02 17:47:00 +0700

    menentukan ganjil genap

---
+++ learning_2/ganjil_genap.js
@@ -0,0 +1,21 @@
+
+    // var num = prompt('Input Angka :');
+    var angka = -7;
+    var hasil = '';
+    var temp = '';
+
+    //Penentu Ganjil Genap
+    if(angka % 2 === 0){
+        hasil = 'Genap';
+    }else{
+        hasil = 'Ganjil';
+    }
+
+    //Penentu Positif Negatif
+    if(angka > 0){
+        temp = 'Positif';
+    }else{
+        temp = 'Negatif';
+    }
+
+    console.log('Angka yang Anda input <b>' +angka+ '</b> adalah <b>' +hasil+ '</b> dan <b>' +temp+ '</b>');
