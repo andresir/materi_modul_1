5160fe6e8597f2c10a940763f014c92ef0694cb2
andresir <bisnis.ir@gmail.com>
2018-12-06 11:06:41 +0700

    cara1, hitung jumlah total loop terakhir di rowSumOddNumbers

--- cara1_rowSumOddNumbers_hitung_jml_loop_akhir.js
+++ cara1_rowSumOddNumbers_hitung_jml_loop_akhir.js
@@ -17,26 +17,3 @@ function rowSum(num){
 }
 
 console.log(rowSum(5))
-
-
-
-// CARA 2 - BARON
-// var rowSumOddNumbers = (num) => {
-//     var arr = [];
-//     var count =1;
-//     var sum = 0;
-//     for(var i=1; i<=5; i++){
-//         var newArr = [];
-//         for(var j=0; j<i; j++){
-//             newArr.push(count)
-//             count+=2;
-//         }
-//         arr.push(newArr)
-//     }
-//     for(var i=0; i< arr[num-1].length; i++){
-//         sum += arr[num-1][i]
-//     }
-//     return sum;
-// }
-
-// console.log(rowSumOddNumbers(5))
