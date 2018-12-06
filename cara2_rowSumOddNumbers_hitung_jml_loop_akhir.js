97d3fdef423b525a3c48f23976bae3d6dda193d7
bigzero <lulagi88@gmail.com>
2018-12-06 10:34:11 +0700

    cara 2, hitung jumlah total loop terakhir di rowSumOddNumbers

---
+++ cara2_rowSumOddNumbers_hitung_jml_loop_akhir.js
@@ -0,0 +1,20 @@
+// CARA 2
+var rowSumOddNumbers = (num) => {
+    var arr = [];
+    var count =1;
+    var sum = 0;
+    for(var i=1; i<=5; i++){
+        var newArr = [];
+        for(var j=0; j<i; j++){
+            newArr.push(count)
+            count+=2;
+        }
+        arr.push(newArr)
+    }
+    for(var i=0; i< arr[num-1].length; i++){
+        sum += arr[num-1][i]
+    }
+    return sum;
+}
+
+console.log(rowSumOddNumbers(5))
